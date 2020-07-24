a73eb3158cbec0a5fce676fe494b40f7303024e6
Jessica <contact.jessicasanchez@gmail.com>
2020-07-24 15:55:23 -0400

    added state to landing for new or returning user

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import { useState } from 'react';
+import Heading from './Heading';
+import './Styles/landing.css'
 
 const Landing = () => {
 
-  
+  const [activeUser, setUser] = useState("SunriseChef")
 
   return (
     <div>
-      <Heading 
-      name="landingWelcome" 
-      content={
-        <span>
-          Welcome, {this.activeUser}
-        </span>
-      }
-      />
+      <div className="landingContainer">
+        <Heading 
+        name="landingWelcome" 
+        content={
+          <span>
+            Welcome back, {activeUser}
+          </span>
+        }
+        />
+      </div>
     </div>
   );
 };
