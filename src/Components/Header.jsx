8487b2dfd4049c52827c31d63d4b0a37b9db3cbd
Jessica <contact.jessicasanchez@gmail.com>
2020-07-25 12:14:51 -0400

    styled header, icon, font

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import "./Styles/header.css"
 const Header = () => {
   return (
     <div>
-      
+      <header className="headerContainer">
+        <img className="logo" src={require("./assets/images/cameraLogoBlack.webp")} alt="black camera"/>
+        <span className="logoName">Momentka</span>
+      </header>
     </div>
   );
 };
---
+++ src/Components/postCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const postCard = () => {
+  return (
+    <div>
+      {/* header -> avatar and username */}
+
+      {/* image */}
+
+      {/* username + caption */}
+    </div>
+  );
+};
+
+export default postCard;
