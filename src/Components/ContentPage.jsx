77e784f0089ef8cb9e83cfd89f42a331126a97f6
Jessica <contact.jessicasanchez@gmail.com>
2020-07-25 17:49:03 -0400

    settng up firebase

--- src/Components/ContentPage.jsx
+++ src/Components/ContentPage.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Header from './Header';
 import Post from './Post';
+import { useEffect } from 'react';
+import {database} from '../fireBase';
 
 const ContentPage = (props) => {
   const [posts, setPosts] = useState([
@@ -16,8 +18,15 @@ const ContentPage = (props) => {
       image: require("./assets/images/food2.jpg"),
       caption: "wow food!"
     },
-    
   ]);
+
+  //useEffect -> runs code with specific conditions
+  useEffect(() => {
+    //code
+    database.collection('Posts').onSnapshot(snapShot) //onSnapshot is a listener. anytime the Posts database is modified it takes a snapshot this will fire.
+  
+  }, []); //<< adding [] means run this code only once. Why does it mean that tho?
+
   return (
     <div>
       <Header/>
