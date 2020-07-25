af8803f260b1fa6bd3f991b50373751caf225853
Jessica <contact.jessicasanchez@gmail.com>
2020-07-25 15:46:40 -0400

    mapped posts to display

--- src/Components/ContentPage.jsx
+++ src/Components/ContentPage.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Post from './Post';
 
 const ContentPage = (props) => {
+  const [posts, setPosts] = useState([
+    {
+      username: props.activeUser,
+      imgName: "post__image",
+      image: require("./assets/images/food1.jpg"),
+      caption: "wow food!"
+    },
+    {
+      username: props.activeUser,
+      imgName: "post__image",
+      image: require("./assets/images/food2.jpg"),
+      caption: "wow food!"
+    },
+    
+  ]);
   return (
     <div>
       <Header/>
-      <Post
-        username={props.activeUser}
-        imgName="post__image1"
-        image={require("./assets/images/food1.jpg")}
-        caption="wow food!"
-        />
-      {/* <Post/>
-      <Post/>
-      <Post/> */}
+      {
+        posts.map(post => (
+          <Post 
+            username={post.username}
+            imgName={post.imgName}
+            image={post.image}
+            caption= {post.caption}
+          />
+        ))
+      }
     </div>
   );
 };
